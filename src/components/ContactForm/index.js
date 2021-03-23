9e22c91a5705e68b1b1889366a26785c7bb6c87a
AgeBK <age24@hotmail.com>
2021-03-23 21:09:54 +1100

    added to home,  contactform, data

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,18 +1,133 @@
 import React from "react";
 // import products from '../../data/products.json';
 import styles from "./ContactForm.module.css";
-import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import ContactDetails from "../../components/ContactDetails";
 
 const ContactForm = () => {
-  //   const [category, setProduct] = useState([]);
-  //   const [categories, setCategories] = useState({});
+  const { register, errors, handleSubmit, reset } = useForm();
+
+  const onSubmit = async (data) => {
+    console.log("Name: ", data.name);
+    console.log("Email: ", data.email);
+    console.log("Subject: ", data.subject);
+    console.log("Message: ", data.message);
+  };
 
   return (
-    <div className={styles.container}>
-      <div className={styles.ContactForm}>
-        <ContactForm>
-            <ul></ul>
-        </ContactForm>
+    <div className="XXXXX">
+      <ContactDetails className="contactForm" />
+      <div className={styles.contactForm}>
+        <form id="contactForm" onSubmit={handleSubmit(onSubmit)} noValidate>
+          {/* Row 1 of form */}
+          <div className={styles.row}>
+            <div className={styles.tb}>
+              <input
+                type="text"
+                name="your name"
+                ref={register({
+                  required: {
+                    value: true,
+                    message: "Please enter your name",
+                  },
+                  maxLength: {
+                    value: 30,
+                    message: "Please use 30 characters or less",
+                  },
+                })}
+                className={styles.formInput}
+                // className="form-control formInput" // TODO is this for the control to work
+                placeholder="Name"
+              ></input>
+              {errors.name && (
+                <span className="errorMessage">{errors.name.message}</span>
+              )}
+            </div>
+            <div className={styles.tb}>
+              <input
+                type="tel"
+                name="phone"
+                // ref={register({
+                //   required: true,
+                //   pattern: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                // })}
+                // className="form-control formInput" // TODO is this for the control to work
+                className={styles.formInput}
+                placeholder="Phone number"
+              ></input>
+              {errors.phone && (
+                <span className="errorMessage">
+                  Please enter a valid phone address
+                </span>
+              )}
+            </div>
+            <div className={styles.tb}>
+              <input
+                type="email"
+                name="email"
+                ref={register({
+                  required: true,
+                  pattern: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                })}
+                className={styles.formInput}
+                // className="form-control formInput" // TODO is this for the control to work
+                placeholder="Email address"
+              ></input>
+              {errors.email && (
+                <span className="errorMessage">
+                  Please enter a valid email address
+                </span>
+              )}
+            </div>
+            {/* Row 2 of form */}
+            {/* <div className={styles.tb}>
+          <input
+            type="text"
+            name="subject"
+            ref={register({
+              required: {
+                value: true,
+                message: "Please enter a subject",
+              },
+              maxLength: {
+                value: 75,
+                message: "Subject cannot exceed 75 characters",
+              },
+            })}
+            className={styles.formInput}
+            // className="form-control formInput" // TODO is this for the control to work
+            placeholder="Subject"
+          ></input>
+          {errors.subject && (
+            <span className="errorMessage">{errors.subject.message}</span>
+          )}
+        </div> */}
+          </div>
+          {/* Row 3 of form */}
+          <div className={styles.row}>
+            <div className={styles.ta}>
+              <textarea
+                rows={3}
+                name="message"
+                ref={register({
+                  required: true,
+                })}
+                className={styles.formInput}
+                // className="form-control formInput" // TODO is this for the control to work
+                placeholder="Message"
+              ></textarea>
+              {errors.message && (
+                <span className="errorMessage">Please enter a message</span>
+              )}
+            </div>
+            <button className={styles.submit} type="submit">
+              Submit Message
+            </button>
+            {/* <div className={styles.test}>
+          <div className={styles.test2}>XXX</div>
+        </div> */}
+          </div>
+        </form>
       </div>
     </div>
   );
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,39 +1,38 @@
 import React from "react";
-// import products from '../../data/products.json';
+import { homeIntro, homeSpecialise } from "../../data/data.json";
 import styles from "./Home.module.css";
 import { Link } from "react-router-dom";
 
 const Home = () => {
-  //   const [category, setProduct] = useState([]);
-  //   const [categories, setCategories] = useState({});
+  const createMarkup = (val) => ({ __html: val });
 
   return (
     <div className={styles.container}>
       <div className={styles.home}>
         <hr />
-        <div className={styles.intro}>
+        <div className={styles.content}>
           {/* j&e Kogan based text */}
-          <h1>Manly Warringah Painters</h1>
-          <strong>
-            Delivering Strata, Commercial and Residential Painting Services for
-            over 25 years
-          </strong>
-          <p>
-            Manly Warringah Painters were established on the Northern Beaches in
-            1995 by Alan Taylor and Frank Edwards and still going strong today.
-            We are experienced in all types of painting and painted finishes.
-            Alan and Frank provide a friendly, professional service at a very
-            competitive price.
-            {/* Please call us today for an obligation free quote */}
-          </p>
-          <p>
-            As painting professionals we’re happy to quote on any size job. We
-            have painters on staff to service the largest commercial project,
-            but willingly undertake smaller jobs. Despite the company expanding
-            to its current formidable size, we have consciously maintained our
-            core ethos of offering a personalised and informed service to all
-            our clients.
-          </p>
+          {/* // TODO: map of areas mwp work and that over 25 years image and error page? */}
+          {/* TODO: semantics are missing, SEO etc, check image 6 slideshow*/}
+          <div dangerouslySetInnerHTML={createMarkup(homeIntro)}></div>
+          <div className={styles.painting}>
+            {homeSpecialise.map((val, i) => (
+              <div key={i} className={styles.paintCont}>
+                <div className={styles.imgCont}>
+                  <img
+                    src={require("../../img/" + val.image)}
+                    alt="Manly Warringah Painting"
+                  />
+                </div>
+                <div className={styles.textCont}>
+                  <div className={styles.textInner}>
+                    <h2 dangerouslySetInnerHTML={createMarkup(val.title)}></h2>
+                    <div dangerouslySetInnerHTML={createMarkup(val.text)}></div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
           <p>
             Although based on the Northern Beaches of Sydney, we provide our
             services to other areas in Sydney. If you’re interested in a quote,
@@ -42,15 +41,10 @@ const Home = () => {
           <p>
             So for quality painting services, you can’t beat the experience,
             professionalism and personal attention of Manly Warringah Painters.
+            We are ready to answer your questions about your painting needs.
+            Please call us for a free, no obligation written estimate at
           </p>
         </div>
-        <p>
-          {/* Established in 1995 on the northern beaches,we are an award winning
-          small team ,experienced in exterior and interior painting ,licensed
-          ,insured and competitively priced,so for an obligation free quote give
-          Alan a call on 0412 128 847 */}
-        </p>
-        <hr />
       </div>
     </div>
   );
